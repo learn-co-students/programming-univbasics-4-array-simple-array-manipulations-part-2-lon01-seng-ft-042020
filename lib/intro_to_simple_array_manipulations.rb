4b1112baf03a6b22c9a92f538ea317a0393b1573
Ewelina Szoda <ewelinaszoda@gmail.com>
2020-04-10 22:49:00 +0000

    Done.

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -6,22 +6,22 @@ def using_insert(array, element)
   array.insert(4, element)
 end 
 
+#the method removes any duplicate items
 def using_uniq(array)
-  #the method removes any duplicate items
   array.uniq 
 end 
 
+#the method contains other arrays and uses the flatten method to return an array of strings
 def using_flatten(array)
-  #the method contains other arrays and uses the flatten method to return an array of strings
   array.flatten 
 end 
 
+#the method removes any items from the array that are equal to the string
 def using_delete(array, string)
-  #the method removes any items from the array that are equal to the string
   array.delete(string)
 end 
 
+# the method deletes the element at the index of the array that is equal to the integer
 def using_delete_at(array, integer)
-  # the method deletes the element at the index of the array that is equal to the integer
   array.delete_at(integer)
 end 
